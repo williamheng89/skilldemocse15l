22f014dd3e9693d015d66e27c0d52009e5d724c6
williamheng89 <williamheng89@gmail.com>
2022-02-04 01:54:40 -0800

    first commit!

---
+++ demoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class demoTest {
+    @Test
+    public void multiply(){
+        assertEquals(3, demo.multiplication(3, 4));
+    }
+}
